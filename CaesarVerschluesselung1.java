7f4cf702903b720a982346e277e4deab1450fd8a
luca- <luca-@192.168.0.4>
2018-11-11 11:44:12 +0100

    -added an if-statement to the methods verschluesseln() and entschluesseln() so that special characters (e.g. ! ? . etc.)  do not get encrypted/decrypted

--- CaesarVerschluesselung1.java
+++ CaesarVerschluesselung1.java
@@ -6,7 +6,7 @@ public class CaesarVerschluesselung1 {
 		String text = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Bitte geben Sie zuerst den Text ein, der ver- oder entschluesselt werden soll! (nur Buchstaben von A-Z, keine Umlaute)");
-		text = input.next();
+		text = input.nextLine();
 		text = text.toUpperCase();
 		System.out.println("Möchten Sie:");
 		System.out.println("(1) den eingegebenen Text verschluesseln oder");
@@ -27,18 +27,21 @@ public class CaesarVerschluesselung1 {
 
 	public static void verschluesseln(String text) {
 		String Textverschluesselt = "";
+		char zeichen = ' ';
 		System.out.println("Bitte geben Sie den Schlüssel als Zahl ein (Um wieviele Stellen soll das Alphabet verschoben werden? z.B. um 3 Stellen --> A wird zu D)");
 		int verschiebung = eingabeSchluessel();
 		
 		
 		for (int i = 0; i < text.length(); i++) {
 			int a = text.charAt(i);
+			if (a >= 65 && a <= 90) { //Wenn das Zeichen kein Buchstabe, sondern z.B. ein Ausrufezeichen oder Leerzeichen ist, soll es nicht verschlüsselt werden, sondern weiterhin im Klartext erscheinen
 			int b = a + verschiebung;
 			if (b > 90) {			//der Unicode als Dezimalzahl für den letzten Großbuchstaben im Alphabet (Z) ist 90; wenn die Zahl darüber liegt, muss man also wieder zum Anfang des Alphabets zurückspringen (Z um 1 nach rechts verschoben ist A) https://unicode-table.com/de/#control-character
-
 				b = b - 26;
 			}
-			char zeichen = (char) b;
+			zeichen = (char) b; } else {
+				zeichen = text.charAt(i);
+			}
 			Textverschluesselt = Textverschluesselt + zeichen;
 		}
 		System.out.println("Entschlüsselter Text: " + Textverschluesselt);
@@ -47,16 +50,20 @@ public class CaesarVerschluesselung1 {
 
 	public static void entschluesseln(String text) {
 		String Textentschluesselt = "";
+		char zeichen = ' ';
 		System.out.println("Bitte geben Sie den Schlüssel ein (Um wieviele Stellen wurde das Alphabet bei der Verschlüsselung verschoben?)");
 		int verschiebung = eingabeSchluessel();
 		
-		for (int i = 0; i < text.length(); i++) {
+		for (int i = 0; i < text.length(); i++) { 
 			int a = text.charAt(i);
+			if (a >= 65 && a <= 90) { //da Sonderzeichen bei der Verschlüsselung nicht verschoben werden, dürfen diese auch bei der Entschlüsselung nicht zurückverschoben werden
 			int b = a - verschiebung;
 			if (b < 65) {   //der Unicodewert als Zahl für den ersten Großbuchstaben im Alphabet (A) ist 65; wenn die Zahl durch die Verschiebung darunter liegt, muss man ans Ende des Alphabets zurückspringen (A um 1 nach links verschoben ist Z)  https://unicode-table.com/de/#control-character
 			b = b + 26;   
 			}
-			char zeichen = (char) b;
+			zeichen = (char) b; } else {
+				zeichen = text.charAt(i);
+			}
 			Textentschluesselt = Textentschluesselt + zeichen;
 	}
 		System.out.println("Entschlüsselter Text: " + Textentschluesselt);
